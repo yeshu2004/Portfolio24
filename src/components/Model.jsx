3d127da3cf0c15b4fb7ba65d4baca9238d6f9103
Yeshu <playeryeshu@gmail.com>
2024-10-09 19:40:52 +0530

    Fix wireframe animation in Model component

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -9,7 +9,7 @@ import PPfont from '../assets/PPGatwick-Regular.ttf'
 const Model = () => {
     const { scrollYProgress } = useScroll()
     const rotationY = useTransform(scrollYProgress, [0, 1], [0, Math.PI * 2])
-    // const scale = useTransform(scrollYProgress, [0, 1], [0, 2])
+    const opacity = useTransform(scrollYProgress, [0,1], [0, 1])
 
     useEffect(() => {
         console.log(scrollYProgress)
@@ -21,28 +21,45 @@ const Model = () => {
             <div className='w-full h-[100vh] sticky top-0'>
                 <Canvas>
                 <ambientLight intensity={0.5} />
-                <motion.group rotation-y={rotationY}rotation={[0,0,0]}>
-                    <mesh>
-                        <boxGeometry args={[3.5, 3.5, 3.5, 10, 10, 10]} />
-                        <meshStandardMaterial
+                <motion.group rotation-y={rotationY} rotation={[0,0,0]}>
+                    <motion.mesh>
+                        <boxGeometry args={[3.5, 3.5, 3.5, 10, 5, 10]} />
+                        <motion.meshStandardMaterial
                             side={DoubleSide}
                             wireframe={true}
                             color={'white'}
-                            antialiasing={true}
+                            transparent={true}
+                            opacity={opacity}
                         />
-                    </mesh>
+                    </motion.mesh>
                 </motion.group>
                 </Canvas>
             </div>
             <div className="relative z-10 px-10">
-                {[1, 2, 3, 4].map((index) => (
+                {[{
+                    title: "FRONT-END Mastery",
+                    content: "Proficient in creating dynamic and responsive user interfaces using React.js, Tailwind."
+                },
+                {
+                    title: "MERN Stack Developer",
+                    content: "Skilled in building full-stack applications using MongoDB, Express.js, React, and Node.js."
+                },
+                {
+                    title: "UI/UX Focus",
+                    content: "Passionate about creating intuitive and visually appealing user experiences."
+                },
+                {
+                    title: "WEB Technologies",
+                    content: "Well-versed in ES6+, TypeScript, and Next.js for building efficient and scalable web applications."
+                }
+                ].map((item, index) => (
                     <div
                         key={index}
                         className={`h-screen flex items-center ${index % 2 === 0 ? 'justify-end' : 'justify-start'}`}
->
-                        <div className="bg-transparent text-white p-4 py-5 border-[1px] border-zinc-300 font-[PPfont]">
-                            <h2 className="text-xl font-bold mb-4">Text Box {index}</h2>
-                            <p>This is the content for text box {index}.</p>
+                    >
+                        <div className="bg-transparent text-white p-4 py-5 font-[PPfont] max-w-sm">
+                            <h2 className="text-2xl font-bold mb-4">{item.title}</h2>
+                            <p className=''>{item.content}</p>
                         </div>
                     </div>
                 ))}
